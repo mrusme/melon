526ebfb96a4747d89f6b2f6bc7aded877e671ac0
Marius <marius@twostairs.com>
2019-04-16 20:34:53 +0200

    Added new resources

--- melon.py
+++ melon.py
@@ -7,6 +7,8 @@ import falcon
 from middleware_json import MiddlewareJson
 
 from resource_api import ResourceApi
+from resource_shortcuts import ResourceShortcuts
+from resource_shortcuts import ResourceShortcutTriggers
 
 class Melon:
     def __init__(self):
@@ -16,6 +18,12 @@ class Melon:
 
         self._api = ResourceApi()
         self._app.add_route('/api', self._api)
+
+        self._shortcuts = ResourceShortcuts()
+        self._app.add_route('/shortcuts/{shortcut_id}', self._shortcuts)
+
+        self._shortcuts = ResourceShortcutTriggers(api=self._api)
+        self._app.add_route('/triggers/{shortcut_id}', self._shortcuts)
 
 melon = Melon()
 app = melon._app
